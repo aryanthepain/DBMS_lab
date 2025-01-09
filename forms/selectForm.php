da979baecd5e4637eadaff5790c6bb4a600e6beb
aryanthepain <aryan.gupta@iitg.ac.in>
2025-01-09 22:32:07 +0530

    select form front end

---
+++ forms/selectForm.php
@@ -0,0 +1,58 @@
+<div class="sellform">
+    <h1 class="sftitle">Search in the DataBase</h1>
+
+    <div class="sfcontainer">
+        <form
+            class="sform"
+            action="include/handleSelect.inc.php"
+            method="post"
+            id="sellform">
+            <div>
+                <div>
+                    <div>
+                        <label class="sflabel" htmlFor="sfaddress">
+                            Roll Number
+                        </label>
+                        <input
+                            type="number"
+                            class="sfinput"
+                            id="sfaddress"
+                            name="roll_no" />
+                    </div>
+                </div>
+
+                <h3>
+                    OR
+                </h3>
+
+                <div>
+                    <div>
+                        <label class="sflabel" htmlFor="sfaddress">
+                            First Name
+                        </label>
+                        <input
+                            type="text"
+                            class="sfinput"
+                            id="sfaddress"
+                            name="firstName" />
+                    </div>
+                    <div>
+                        <label class="sflabel" htmlFor="sftype">
+                            Last Name
+                        </label>
+                        <input
+                            type="text"
+                            class="sfinput"
+                            id="sftype"
+                            name="lastName" />
+                    </div>
+                </div>
+
+
+
+            </div>
+
+            <input type="submit" value="Search" class="sfsubmit" />
+        </form>
+    </div>
+</div>
