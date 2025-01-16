f73feb96b50ad4155961956b0f6e698d91e31422
aryanthepain <aryan.gupta@iitg.ac.in>
2025-01-16 21:24:53 +0530

    initialise code changed to delete student info

--- initialiseDB.sql
+++ initialiseDB.sql
@@ -14,9 +14,9 @@ CREATE TABLE
     has_issued (
         transaction_id INT PRIMARY key NOT NULL auto_increment,
         book_id INT (5) NOT NULL,
-        Roll_number INT (9) NOT NULL,
+        Roll_number INT (9),
         issue_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
         return_date DATETIME NULL,
-        FOREIGN key (book_id) REFERENCES books (book_id),
-        FOREIGN key (Roll_number) REFERENCES students (Roll_number)
+        FOREIGN key (book_id) REFERENCES books (book_id) ON DELETE CASCADE,
+        FOREIGN key (Roll_number) REFERENCES students (Roll_number) ON DELETE SET NULL
     );
