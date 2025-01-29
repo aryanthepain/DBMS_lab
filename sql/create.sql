a55c063b0fcd64889f3bac34114f9bd4b7e23348
aryanthepain <aryan.gupta@iitg.ac.in>
2025-01-29 14:42:11 +0530

    new tabole created

---
+++ sql/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE
+    students (
+        Roll_number BIGINT PRIMARY key auto_increment NOT NULL,
+        name VARCHAR(25) NOT NULL,
+        photo VARCHAR(10) NOT NULL,
+        Phone_no INT (10) NOT NULL
+    );
