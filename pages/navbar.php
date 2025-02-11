b94febdd602359c6f3752a09dc3fd01912c851fb
aryanthepain <aryan.gupta@iitg.ac.in>
2025-02-11 23:35:55 +0530

    navbar for all pages

---
+++ pages/navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">Exam Portal</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>
+                <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+                <li class="nav-item"><a class="nav-link" href="exam_registration.php">Exam Registration</a></li>
+                <li class="nav-item"><a class="nav-link" href="booking.php">Booking & Fees</a></li>
+                <li class="nav-item"><a class="nav-link" href="schedule.php">Schedule</a></li>
+                <li class="nav-item"><a class="nav-link" href="exam_portal.php">Take Exam</a></li>
+                <li class="nav-item"><a class="nav-link" href="evaluation.php">Evaluation</a></li>
+                <li class="nav-item"><a class="nav-link" href="dashboard_analysis.php">Dashboard</a></li>
+                <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
