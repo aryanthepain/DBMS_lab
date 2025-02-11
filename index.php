2fe8736ba87dcce3258317f2af072277f5c07fbf
aryanthepain <aryan.gupta@iitg.ac.in>
2025-02-11 23:33:29 +0530

    page moved inside pages folder

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
 <?php
 header('Location: pages/register.php');
 exit;
-
-// <!DOCTYPE html>
-// <html lang="en">
-
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Lab 1</title>
-//     <link rel="stylesheet" href="css/form.css">
-// </head>
-
-// <body margin="0%">
-//     <?php
-//     // all the forms
-//     header('pages/register.php');
-//     // include("forms/selectForm.php");
-//     // include("forms/deleteForm.php");
-//     // include("forms/modifyForm.php");
-//     
-// 
-// </body>
-
-//
-
-// </html>
