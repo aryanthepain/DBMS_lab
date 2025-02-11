fb284ea825d05d7aaf7a6deeb660484da22d2455
aryanthepain <aryan.gupta@iitg.ac.in>
2025-02-11 23:54:09 +0530

    navbar includes admin features

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,3 +1,9 @@
+<?php
+// Start session if not already started.
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-success">
     <div class="container">
         <a class="navbar-brand" href="index.php">Exam Portal</a>
@@ -7,15 +13,26 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>
-                <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
-                <li class="nav-item"><a class="nav-link" href="exam_registration.php">Exam Registration</a></li>
-                <li class="nav-item"><a class="nav-link" href="booking.php">Booking & Fees</a></li>
-                <li class="nav-item"><a class="nav-link" href="schedule.php">Schedule</a></li>
-                <li class="nav-item"><a class="nav-link" href="exam_portal.php">Take Exam</a></li>
-                <li class="nav-item"><a class="nav-link" href="evaluation.php">Evaluation</a></li>
-                <li class="nav-item"><a class="nav-link" href="dashboard_analysis.php">Dashboard</a></li>
-                <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+                <?php if (isset($_SESSION['admin'])): ?>
+                    <li class="nav-item"><a class="nav-link" href="admin_dashboard.php">Dashboard</a></li>
+                    <li class="nav-item"><a class="nav-link" href="exam_registration.php">Exam Registration</a></li>
+                    <li class="nav-item"><a class="nav-link" href="create_timeslot.php">Create Time Slot</a></li>
+                    <li class="nav-item"><a class="nav-link" href="book_exam_slot.php">Book Exam Slot</a></li>
+                    <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+                <?php elseif (isset($_SESSION['roll'])): ?>
+                    <li class="nav-item"><a class="nav-link" href="dashboard.php">Dashboard</a></li>
+                    <li class="nav-item"><a class="nav-link" href="exam_registration.php">Exam Registration</a></li>
+                    <li class="nav-item"><a class="nav-link" href="booking.php">Booking & Fees</a></li>
+                    <li class="nav-item"><a class="nav-link" href="schedule.php">Schedule</a></li>
+                    <li class="nav-item"><a class="nav-link" href="exam_portal.php">Take Exam</a></li>
+                    <li class="nav-item"><a class="nav-link" href="evaluation.php">Evaluation</a></li>
+                    <li class="nav-item"><a class="nav-link" href="dashboard_analysis.php">Dashboard</a></li>
+                    <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+                <?php else: ?>
+                    <li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>
+                    <li class="nav-item"><a class="nav-link" href="login.php">Student Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="admin_login.php">Admin Login</a></li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
