5aaf4c81289337f26c5bafc5b0c152cad6dd2bea
aryanthepain <aryan.gupta@iitg.ac.in>
2025-02-11 23:42:14 +0530

    login page renewed

--- pages/login.php
+++ pages/login.php
@@ -8,18 +8,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $roll = $_POST['roll'];
     $password = $_POST['password'];
 
-    // Prepare and execute
     $stmt = $pdo->prepare("SELECT password FROM student_password WHERE Roll_number = :roll");
     $stmt->bindParam(":roll", $roll);
     $stmt->execute();
     $db_password = $stmt->fetchAll();
 
-
     if ($stmt->rowCount() > 0) {
         if ($password === $db_password[0]["password"]) {
             $message = "Login successful";
             $toastClass = "bg-success";
-            // Start the session and redirect to the dashboard or home page
             session_start();
             $_SESSION['roll'] = $roll;
             $_SESSION['isAdmin'] = false;
@@ -33,9 +30,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $message = "Roll number not found";
         $toastClass = "bg-warning";
     }
-
-    $stmt = null;
-    $pdo = null;
 }
 ?>
 
@@ -44,67 +38,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
-    <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/295/295128.png">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="../css/login.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body class="bg-light">
+    <?php include 'navbar.php'; ?>
     <div class="container p-5 d-flex flex-column align-items-center">
         <?php if ($message): ?>
-            <div class="toast align-items-center text-white 
-            <?php echo $toastClass; ?> border-0" role="alert"
-                aria-live="assertive" aria-atomic="true">
+            <div class="toast align-items-center text-white <?php echo $toastClass; ?> border-0" role="alert" aria-live="assertive" aria-atomic="true">
                 <div class="d-flex">
-                    <div class="toast-body">
-                        <?php echo $message; ?>
-                    </div>
-                    <button type="button" class="btn-close
-                    btn-close-white me-2 m-auto" data-bs-dismiss="toast"
-                        aria-label="Close"></button>
+                    <div class="toast-body"><?php echo $message; ?></div>
+                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
                 </div>
             </div>
         <?php endif; ?>
-        <form action="" method="post" class="form-control mt-5 p-4"
-            style="height:auto; width:380px; box-shadow: rgba(60, 64, 67, 0.3) 
-            0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;">
+        <form action="" method="post" class="form-control mt-5 p-4" style="max-width:380px;">
             <div class="row">
-                <i class="fa fa-user-circle-o fa-3x mt-1 mb-2"
-                    style="text-align: center; color: green;"></i>
-                <h5 class="text-center p-4"
-                    style="font-weight: 700;">Login Into Your Account</h5>
+                <i class="fa fa-user-circle-o fa-3x mt-1 mb-2" style="text-align: center; color: green;"></i>
+                <h5 class="text-center p-4" style="font-weight: 700;">Login Into Your Account</h5>
             </div>
-            <div class="col-mb-3">
-                <label for="roll"><i
-                        class="fa fa-hashtag"></i> Roll Number</label>
-                <input type="text" name="roll" id="roll"
-                    class="form-control" required>
+            <div class="mb-3">
+                <label for="roll"><i class="fa fa-hashtag"></i> Roll Number</label>
+                <input type="text" name="roll" id="roll" class="form-control" required>
             </div>
-            <div class="col mb-3 mt-3">
-                <label for="password"><i
-                        class="fa fa-lock"></i> Password</label>
-                <input type="password" name="password" id="password"
-                    class="form-control" required>
+            <div class="mb-3 mt-3">
+                <label for="password"><i class="fa fa-lock"></i> Password</label>
+                <input type="password" name="password" id="password" class="form-control" required>
             </div>
-            <div class="col mb-3 mt-3">
-                <button type="submit"
-                    class="btn btn-success bg-success" style="font-weight: 600;">Login</button>
+            <div class="mb-3 mt-3">
+                <button type="submit" class="btn btn-success" style="font-weight: 600;">Login</button>
             </div>
-            <div class="col mb-2 mt-4">
-                <p class="text-center"
-                    style="font-weight: 600; color: navy;"><a href="./register.php"
-                        style="text-decoration: none;">Create Account</a></p>
+            <div class="mb-2 mt-4">
+                <p class="text-center" style="font-weight: 600; color: navy;"><a href="register.php" style="text-decoration: none;">Create Account</a></p>
             </div>
         </form>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script>
-        var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-        var toastList = toastElList.map(function(toastEl) {
+        let toastElList = [].slice.call(document.querySelectorAll('.toast'));
+        let toastList = toastElList.map(function(toastEl) {
             return new bootstrap.Toast(toastEl, {
                 delay: 3000
             });
