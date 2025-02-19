362e043b8ae32efc6500ae1c4d74e99edb5b85fb
aryanthepain <aryan.gupta@iitg.ac.in>
2025-02-19 13:40:30 +0530

    display alert added

--- pages/process_schedule.php
+++ pages/process_schedule.php
@@ -1,7 +1,9 @@
 <?php
+// author: aryanthepain
 // file: pages/process_schedule.php
 session_start();
 require_once '../include/dbh.inc.php';
+require_once '../include/displayAlert.inc.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $bookingID = $_POST['bookingID'];
@@ -13,8 +15,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->bindParam(':bookingID', $bookingID, PDO::PARAM_INT);
         $stmt->execute();
 
-        echo "<p>Exam scheduled/rescheduled successfully for booking ID: $bookingID with slot ID: $selectedSlotID.</p>";
-        echo "<p><a href='dashboard.php'>Return to Dashboard</a></p>";
+        $alert = 'Exam scheduled/rescheduled successfully for booking ID: ' . $bookingID . ' with slot ID: ' . $selectedSlotID . '\nReturn to Dashboard';
+        display_alert($alert, 'dashboard.php');
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
