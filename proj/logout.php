331025ae50c5fbd547f0a97636d9fae7c661e5da
aryanthepain <aryan.gupta@iitg.ac.in>
2025-04-16 23:57:57 +0530

    extra project deleted

--- proj/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ATP - logout.php
-// Logs the user out by clearing session variables and destroying the session.
-session_start();
-session_unset();
-session_destroy();
-header("Location: index.php");
-exit();
